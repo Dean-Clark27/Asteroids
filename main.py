446697d25e5325f55c471b13d9f25af95747dc77
Dean-Clark27 <dmc227@lehigh.edu>
2024-12-23 11:41:04 -0500

    Adds a player, renders them to the screen, and allows them to rotate to the left/right using the a and d keys

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS, 0)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     while True:
         screen.fill("black", rect=None, special_flags=0)
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
