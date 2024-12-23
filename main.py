5c3dfe54c400975066ac4a9c73dbe2a7c243728b
Dean-Clark27 <dmc227@lehigh.edu>
2024-12-23 16:20:18 -0500

    allows for all movement of the character in proper groups

--- main.py
+++ main.py
@@ -6,13 +6,19 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS, 0)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS, 0)
+    
     
     while True:
         screen.fill("black", rect=None, special_flags=0)
-        player.update(dt)
-        player.draw(screen)
+        for u in updatable:
+            u.update(dt)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
